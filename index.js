258f6ff87beab3b0166d54d3439f6e4a974e06c6
Seunggyu <seunggyul14@icloud.com>
2022-10-26 15:02:51 -0700

    commit tree f9538053faac14a2997edcd17faa3d16aef30fd1

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const del = document.querySelector('main#main')
+del.remove();
+
+const newHeader = document.createElement("h1")
+newHeader.setAttribute('id', 'victory');
+newHeader.textContent ="bob is the champion"
